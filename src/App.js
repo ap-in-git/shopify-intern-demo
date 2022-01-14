a3db0d05651a02a9d84405a8c5d3e51214b3f703
ashok <hi@apinweb.com>
2022-01-14 15:59:34 -0500

    Api url images moved to const

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import './App.css';
 import {Page} from "@shopify/polaris";
 import { useEffect, useState} from "react";
 import ImageCard from "./components/ImageCard";
+const API_URL = "https://api.nasa.gov/planetary/apod?api_key=7kKAmNa91CqlNCkmMFahjvbVn4tGvJRjotFaeqlF&start_date=2022-01-01&end_date=2022-01-14"
 function App() {
     const [images,setImages] = useState([]);
     const fetchData = async () => {
-        const url = "https://api.nasa.gov/planetary/apod?api_key=7kKAmNa91CqlNCkmMFahjvbVn4tGvJRjotFaeqlF&start_date=2022-01-01&end_date=2022-01-14"
-        const response = await fetch(url).then(response => response.json())
-        console.log(response)
+        const response = await fetch(API_URL).then(response => response.json())
         setImages(response)
     }
     useEffect(() => {
